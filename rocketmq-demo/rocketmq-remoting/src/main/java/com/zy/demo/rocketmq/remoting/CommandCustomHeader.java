cd7788f78fa8f4668db8bcb79ab54c5f574de4f8
zy <602128569@qq.com>
2019-01-06 11:53:25 +0800

    add createResponseCommand

---
+++ rocketmq-demo/rocketmq-remoting/src/main/java/com/zy/demo/rocketmq/remoting/CommandCustomHeader.java
@@ -0,0 +1,9 @@
+package com.zy.demo.rocketmq.remoting;
+
+/**
+ * 自定义命令header
+ * 数据头
+ */
+public interface CommandCustomHeader {
+    void checkFields();
+}
--- rocketmq-demo/rocketmq-remoting/src/main/java/com/zy/demo/rocketmq/remoting/protocol/RemotingCommand.java
+++ rocketmq-demo/rocketmq-remoting/src/main/java/com/zy/demo/rocketmq/remoting/protocol/RemotingCommand.java
@@ -1,10 +1,20 @@
 package com.zy.demo.rocketmq.remoting.protocol;
 
+import com.zy.demo.rocketmq.remoting.CommandCustomHeader;
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Getter
+@Setter
 public class RemotingCommand {
+    public static final String SERIALIZE_TYPE_PROPERTY = "rocketmq.serialize.type";
+    public static final String SERIALIZE_TYPE_ENV = "ROCKETMQ_SERIALIZE_TYPE";
+    public static final String REMOTING_VERSION_KEY = "rocketmq.remoting.version";
+
     private static final String STRING_CANONICAL_NAME = String.class.getCanonicalName();
     private static final String DOUBLE_CANONICAL_NAME_1 = Double.class.getCanonicalName();
     private static final String DOUBLE_CANONICAL_NAME_2 = double.class.getCanonicalName();
@@ -19,7 +29,8 @@ public class RemotingCommand {
 
     //todo
     static {
-        final String protocol = System.getProperty("", System.getenv(""));
+        //从系统中获取属性值，
+        final String protocol = System.getProperty(SERIALIZE_TYPE_PROPERTY, System.getenv(SERIALIZE_TYPE_ENV));
         if (StringUtils.isNoneBlank(protocol)){
             try {
                 serializeTypeConfigInTheisServer = SerializeType.valueOf(protocol);
@@ -29,10 +40,56 @@ public class RemotingCommand {
         }
     }
 
+    private int code;
+    private LanguageCode languageCode = LanguageCode.JAVA;
+    private int version = 0;
+
+    private static volatile int configVersion = -1;
     private AtomicInteger requestId = new AtomicInteger(0);
     private int opaque = requestId.getAndIncrement();
     private int flag = 0;
+    private String remark;
+    private HashMap<String, String> extFields;
 
-    public static final String SERIALIZE_TYPE_PROPERTY = "rocketmq.serialize.type";
+    protected RemotingCommand() {}
+
+    public static RemotingCommand createRequestCommand(int code, CommandCustomHeader customHeader){
+        RemotingCommand cmd = new RemotingCommand();
+
+        return cmd;
+    }
+
+    /**
+     * todo:
+     * Class<? extends CommandCustomHeader> classHeader 和 CommandCustomHeader 的区别是什么
+     * @param classHeader
+     * @return
+     */
+    public static RemotingCommand createResponseCommand(Class<? extends CommandCustomHeader> classHeader){
+        return createResponseCommand(RemotingSysReponseCode.SYSTEM_ERROR, "not set any response code", classHeader);
+    }
+
+    public static RemotingCommand createResponseCommand(int code, String remark, Class<? extends CommandCustomHeader> classHeader){
+        return null;
+    }
+
+    public static RemotingCommand createResponseCommand(int code, String remark){
+        return createResponseCommand(code, remark, null);
+    }
+
+    private static void setCmdVersion(RemotingCommand cmd){
+        if (configVersion >= 0){
+            cmd.setVersion(configVersion);
+        } else {
+            String v = System.getProperty(REMOTING_VERSION_KEY);
+            if (v != null){
+                int value = Integer.parseInt(v);
+                configVersion = value;
+                cmd.setVersion(value);
+            }
+        }
+    }
+
+
 
 }
