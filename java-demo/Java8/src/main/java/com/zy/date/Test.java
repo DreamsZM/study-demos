f4e84e8921fd56a8e2b3e340767e77f9c25f4ab0
zy <602128569@qq.com>
2019-03-19 11:25:20 +0800

    test:

--- java-demo/Java8/src/main/java/com/zy/date/Test.java
+++ java-demo/Java8/src/main/java/com/zy/date/Test.java
@@ -1,10 +1,18 @@
 package com.zy.date;
 
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Test {
     public static void main(String[] args) {
-        String dateStr = DateUtils.format(new Date());
-        System.out.println(dateStr);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(() -> {
+                String dateStr = DateUtils.format(new Date());
+                System.out.println(dateStr);
+            });
+        }
     }
 }
